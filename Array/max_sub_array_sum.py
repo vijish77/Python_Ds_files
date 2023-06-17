067e3af8c379ffcc5ff1aa6bd42b6018443c10a0
Vijeesh Koyappayil <vijish77@gmail.com>
2023-06-17 11:14:24 +0530

    maximum subaray problem

--- Array/max_sub_array_sum.py
+++ Array/max_sub_array_sum.py
@@ -1,4 +1,4 @@
-#Max sub arrays sum
+# Max sub arrays sum
 #
 # Note
 # Total no of subarrays N(N+1)/2
@@ -6,12 +6,18 @@
 
 arr = [-7, 4, 3, -2, -8, 6, -4, 2]
 
-res = float('-inf')
-for i in range(0,len(arr)):
-    for j in range(i+1, len(arr)):
-        sum_= sum(arr[i:j])
-        if sum_ > res:
-            res = sum_
-print(res)
+ans = float('-inf')
+for i in range(0, len(arr)):
+    for j in range(i + 1, len(arr)):
+        sum_ = sum(arr[i:j])
+        if sum_ > ans:
+            ans = sum_
+print(ans)
 
-
+ans = float('-inf')
+for i in range(0, len(arr)):
+    sum_ = 0
+    for j in range(i, len(arr)):
+        sum_ += arr[j]
+        ans = max(sum_, ans)
+print(ans)
